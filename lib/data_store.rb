TangliziGit/lkp-tests

commit 9e624cd599f545ba86d694addd45aab0c2159422

rubocop: cleanup Style/OptionalBooleanParameter offenses
$ rake code
syntax OK
...
PWD = /home/yujie/upstream/lkp-tests
rubocop.patterns = []
rubocop.options = ["-D", "-c.rubocop.yml"]
Running RuboCop...
Inspecting 267 files
...........................................................................................................................................................................................................................................................................

267 files inspected, no offenses detected
PWD = /home/yujie/upstream/lkp-tests
ENV['LKP_SRC'] = /home/yujie/upstream/lkp-tests
/usr/bin/ruby2.7 -I/var/lib/gems/2.7.0/gems/rspec-core-3.10.1/lib:/var/lib/gems/2.7.0/gems/rspec-support-3.10.2/lib /var/lib/gems/2.7.0/gems/rspec-core-3.10.1/exe/rspec --pattern spec/\*\*\{,/\*/\*\*\}/\*_spec.rb
...
Finished in 2 minutes 11.1 seconds (files took 0.66787 seconds to load)
456 examples, 0 failures, 6 pending

Signed-off-by: Yujie Liu <yujie.liu@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/data_store.rb
@@ -59,7 +59,7 @@ module DataStore
     prop_reader :maps
     prop_accessor :compress_matrix
 
-    def initialize(path, create_new = false)
+    def initialize(path, create_new: false)
       @path = path
       if create_new
         @maps = []
@@ -243,7 +243,7 @@ module DataStore
 
     def create_new(path)
       path = canonicalize_path path
-      cached_new path, path, true
+      cached_new path, path, create_new: true
     end
 
     def exist?(path)
@@ -294,7 +294,7 @@ module DataStore
 
     include DirObject
 
-    def initialize(path, create_new = false)
+    def initialize(path, create_new: false)
       @path = path
       load_config unless create_new
     end
@@ -626,7 +626,7 @@ module DataStore
       @create_time
     end
 
-    def index(force = false)
+    def index(force: false)
       indexed_file = path(INDEXED_FILE)
       FileUtils.rm_f(indexed_file) if force
       indexed = File.exist? indexed_file

lib/compare.rb
@@ -1031,7 +1031,7 @@ module Compare
     comparer.compare
   end
 
-  def self.perf_comparer(commits, strict = false)
+  def self.perf_comparer(commits, strict: false)
     ignored_testcases = Set.new %w[xfstests phoronix-test-suite]
     git = axis_key_git COMMIT_AXIS_KEY
     commits = git.sort_commits commits

lib/git_ext/base.rb
@@ -188,12 +188,12 @@ module Git
       command('rev-list --reverse HEAD |head -1')
     end
 
-    def command(cmd, opts = [], chdir = true, redirect = '', &block)
-      lib.command(cmd, opts, chdir, redirect, &block)
+    def command(cmd, opts = [], redirect = '', chdir: true, &block)
+      lib.command(cmd, opts, redirect, chdir: chdir, &block)
     end
 
-    def command_lines(cmd, opts = [], chdir = true, redirect = '')
-      lib.command_lines(cmd, opts, chdir, redirect)
+    def command_lines(cmd, opts = [], redirect = '', chdir: true)
+      lib.command_lines(cmd, opts, redirect, chdir: chdir)
     end
   end
 end

lib/git_ext/lib.rb
@@ -7,8 +7,8 @@ require "#{LKP_SRC}/lib/string_ext"
 
 module Git
   class Lib
-    def command_lines(cmd, opts = [], chdir = true, _redirect = '')
-      command_lines = command(cmd, opts, chdir)
+    def command_lines(cmd, opts = [], _redirect = '', chdir: true)
+      command_lines = command(cmd, opts, chdir: chdir)
 
       # to deal with "GIT error: cat-file ["commit", "9f86262dcc573ca195488de9ec6e4d6d74288ad3"]: invalid byte sequence in US-ASCII"
       # - one possibility is the encoding of string is wrongly set (due to unknown reason), e.g. UTF-8 string's encoding is set as US-ASCII
@@ -24,7 +24,7 @@ module Git
 
     ENV_VARIABLE_NAMES = %w(GIT_DIR GIT_WORK_TREE GIT_INDEX_FILE GIT_SSH).freeze unless defined? ENV_VARIABLE_NAMES
 
-    def command(cmd, opts = [], chdir = true, redirect = '', &block)
+    def command(cmd, opts = [], redirect = '', chdir: true, &block)
       system_env_variables = Hash[ENV_VARIABLE_NAMES.map { |name| [name, ENV[name]] }]
 
       orig_command(cmd, opts, chdir, redirect, &block)

lib/hash.rb
@@ -2,7 +2,7 @@
 
 require 'set'
 
-def lookup_hash(hash, path, create_missing = false)
+def lookup_hash(hash, path, create_missing: false)
   keys = path.split('.')
   parent = hash
   pkey = keys.first
@@ -71,7 +71,7 @@ end
 # "overwrite_top_keys = true" will have the same semantics with
 # original.update(revisions) except for the special *+, *-, a.b.c
 # notions and accumulative keys.
-def revise_hash(original, revisions, overwrite_top_keys = true)
+def revise_hash(original, revisions, overwrite_top_keys: true)
   # deal with empty YAML files gracefully
   original ||= {}
   revisions ||= {}
@@ -119,7 +119,7 @@ def revise_hash(original, revisions, overwrite_top_keys = true)
       next false
     when '+'
       kk = k.chomp '+'
-      _parent, _pkey, hash, key, _keys = lookup_hash(original, kk, true)
+      _parent, _pkey, hash, key, _keys = lookup_hash(original, kk, create_missing: true)
       merge_v = merge_accumulative(hash[key], v)
       merge_v.uniq! if merge_v.instance_of? Array
       hash[key] = merge_v
@@ -128,7 +128,7 @@ def revise_hash(original, revisions, overwrite_top_keys = true)
 
     next true unless k.index('.')
 
-    _parent, _pkey, hash, key, _keys = lookup_hash(original, k, true)
+    _parent, _pkey, hash, key, _keys = lookup_hash(original, k, create_missing: true)
     hash[key] = v if overwrite_top_keys || hash.object_id != original.object_id || hash[key].nil?
     next false if hash.object_id != original.object_id
 

lib/hashugar.rb
@@ -54,7 +54,7 @@ class Hashugar
     @table[stringify(key)] = value
   end
 
-  def respond_to?(key, _include_all = false)
+  def respond_to?(key)
     super(key) || @table.key?(stringify(key))
   end
 

lib/job.rb
@@ -199,7 +199,7 @@ class Job
     comment_to_symbol file.sub("#{lkp_src}/", '')
   end
 
-  def load(jobfile, expand_template = false)
+  def load(jobfile, expand_template: false)
     yaml = File.read jobfile
     # give a chance
     if yaml.size.zero? && !File.size(jobfile).zero?
@@ -282,8 +282,8 @@ class Job
     return nil unless File.exist? file
 
     context_hash = deepcopy(@defaults)
-    revise_hash(context_hash, job, true)
-    revise_hash(context_hash, @overrides, true)
+    revise_hash(context_hash, job, overwrite_top_keys: true)
+    revise_hash(context_hash, @overrides, overwrite_top_keys: true)
     begin
       defaults = load_yaml(file, context_hash)
     rescue KeyError
@@ -291,14 +291,14 @@ class Job
     end
     if defaults.is_a?(Hash) && !defaults.empty?
       @defaults[source_file_symkey(file)] = nil
-      revise_hash(@defaults, defaults, true)
+      revise_hash(@defaults, defaults, overwrite_top_keys: true)
     end
     true
   end
 
-  def load_defaults(first_time = true)
+  def load_defaults(first_time: true)
     if @job.include? :no_defaults
-      merge_defaults first_time
+      merge_defaults first_time: first_time
       return
     end
 
@@ -310,14 +310,14 @@ class Job
 
     i = include_files
     job = deepcopy(@job)
-    revise_hash(job, deepcopy(@job2), true)
-    revise_hash(job, deepcopy(@overrides), true)
+    revise_hash(job, deepcopy(@job2), overwrite_top_keys: true)
+    revise_hash(job, deepcopy(@overrides), overwrite_top_keys: true)
     job['___'] = nil
     expand_each_in(job, @dims_to_expand) do |h, k, v|
       h.delete(k) if v.is_a?(Array)
     end
     @jobx = job
-    expand_params(false)
+    expand_params(run_scripts: false)
     @jobx = nil
     for_each_in(job, i.keys) do |_pk, _h, k, v|
       job['___'] = v
@@ -365,23 +365,23 @@ class Job
       load_one['ALL']
     end
 
-    merge_defaults first_time
+    merge_defaults first_time: first_time
   end
 
-  def merge_defaults(first_time = true)
-    revise_hash(@job, @defaults, false)
+  def merge_defaults(first_time: true)
+    revise_hash(@job, @defaults, overwrite_top_keys: false)
     @defaults = {}
 
     return unless first_time
 
-    revise_hash(@job, @job2, true)
+    revise_hash(@job, @job2, overwrite_top_keys: true)
 
     return if @overrides.empty?
 
     key = comment_to_symbol('user overrides')
     @job.delete key
     @job[key] = nil
-    revise_hash(@job, @overrides, true)
+    revise_hash(@job, @overrides, overwrite_top_keys: true)
   end
 
   def save(jobfile)
@@ -483,7 +483,7 @@ class Job
       end
     end
     job = deepcopy self
-    job.load_defaults false
+    job.load_defaults first_time: false
     job.delete :no_defaults
     yield job
   end
@@ -668,7 +668,7 @@ class Job
     raise Job::ParamError, "#{script}: exitstatus #{$CHILD_STATUS.exitstatus}" if $CHILD_STATUS.exitstatus && $CHILD_STATUS.exitstatus != 0
   end
 
-  def expand_params(run_scripts = true)
+  def expand_params(run_scripts: true)
     @jobx ||= deepcopy @job
     maps, ruby_scripts, misc_scripts = param_files
     begin
@@ -822,9 +822,9 @@ class JobEval < Job
 end
 
 class << Job
-  def open(jobfile, expand_template = false)
+  def open(jobfile, expand_template: false)
     j = new
-    j.load(jobfile, expand_template) && j
+    j.load(jobfile, expand_template: expand_template) && j
   end
 end
 

lib/matrix.rb
@@ -170,7 +170,7 @@ def create_stats_matrix(result_root)
   add_performance_per_watt(stats, matrix)
   add_path_length(stats, matrix)
   save_json(stats, "#{result_root}/stats.json")
-  save_json(matrix, "#{result_root}/matrix.json", true)
+  save_json(matrix, "#{result_root}/matrix.json", compress: true)
   if local_run?
     save_matrix_to_csv_file("#{result_root}/stats.csv", stats)
     save_matrix_to_csv_file("#{result_root}/matrix.csv", matrix)
@@ -243,7 +243,7 @@ def unite_remove_empty_stats(matrix)
   matrix.reject { |_k, v| v.empty? }
 end
 
-def unite_to(stats, matrix_root, max_cols = nil, delete = false)
+def unite_to(stats, matrix_root, max_cols = nil, delete: false)
   matrix_file = "#{matrix_root}/matrix.json"
 
   matrix = load_matrix_file("#{matrix_root}/matrix.json")
@@ -335,7 +335,7 @@ def sort_matrix(matrix, key)
   m
 end
 
-def save_matrix_as_csv(file, matrix, sep = ' ', _header = true, fill = -1)
+def save_matrix_as_csv(file, matrix, sep = ' ', fill = -1, _header: true)
   fill && cols = matrix.map { |_k, v| v.size }.max
   matrix.each do |k, vs|
     vs = Array vs
@@ -345,9 +345,9 @@ def save_matrix_as_csv(file, matrix, sep = ' ', _header = true, fill = -1)
   end
 end
 
-def save_matrix_to_csv_file(file_name, matrix, sep = ',', header = true)
+def save_matrix_to_csv_file(file_name, matrix, sep = ',', header: true)
   File.open(file_name, 'w') do |f|
-    save_matrix_as_csv(f, matrix, sep, header, nil)
+    save_matrix_as_csv(f, matrix, sep, nil, _header: header)
   end
 end
 
@@ -438,7 +438,7 @@ def unite_params(result_root)
   end
 end
 
-def unite_stats(result_root, delete = false)
+def unite_stats(result_root, delete: false)
   unless File.directory? result_root
     log_error "#{result_root} is not a directory"
     return false
@@ -454,9 +454,9 @@ def unite_stats(result_root, delete = false)
 
   stats['stats_source'] = "#{result_root}/stats.json"
 
-  unite_to(stats, _result_root, nil, delete)
+  unite_to(stats, _result_root, nil, delete: delete)
   begin
-    __matrix = unite_to(stats, __result_root, 100, delete)
+    __matrix = unite_to(stats, __result_root, 100, delete: delete)
     check_warn_test_error __matrix, result_root
   rescue StandardError => e
     log_warn e.formatted_headline

commit 5953709d9512933e335ddb8c297ec5dec2f82804

lib/data_store.rb: fix 297:in 'initialize': wrong number of arguments
Traceback (most recent call last):
    6: from /lkp-tests/bin/run-local:165:in `<main>'
        5: from /lkp-tests/lib/nresult_root.rb:339:in `create_tables_layout'
        4: from /lkp-tests/lib/nresult_root.rb:257:in `create_layout'
        3: from /lkp-tests/lib/nresult_root.rb:243:in `create_layout'
        2: from /lkp-tests/lib/data_store.rb:225:in `add_index'
        1: from /lkp-tests/lib/data_store.rb:225:in `new'
/lkp-tests/lib/data_store.rb:297:in `initialize': wrong number of arguments (given 2, expected 1) (ArgumentError)

Fixes: 9e624cd599f5 (rubocop: cleanup Style/OptionalBooleanParameter offenses)
Signed-off-by: Chen Rong <rong.a.chen@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/data_store.rb
@@ -222,7 +222,7 @@ module DataStore
       dir = cons_index_path cls, name
       raise "Index already exist: #{dir}" if Dir.exist? dir
 
-      index = cls.new(dir, true)
+      index = cls.new(dir, create_new: true)
       yield index if block_given?
       index.save_config
     end