f03e9357bfa2bfd5a9735a2bc905ad578e2dcd89
Aaron Lu <aaron.lu@intel.com>
2022-04-19 10:45:08 +0800

    nvdimm: make it possible to specify namespace to reconfig
    I would like to use a specified /dev/pmemX as swap device instead of
    using all /dev/pmem devices and for this to work, I need to reconfig the
    specified namespace to let a single /dev/pmemX appear.
    
    E.g, to use /dev/pmem1 which is on node 1 as swap device, the job file
    will have the following:
    
    nvdimm:
      mode: raw
      ns_id: 1
    disk:
      nr_pmem: 1
    swap:
    
    This has the assumption of all optane DIMMs are configured as dax
    devices by default and it *seems* to be the case but I'm not entirely
    sure why and whether this will change in the future.
    
    Cc: Ying Huang <ying.huang@intel.com>
    Signed-off-by: Aaron Lu <aaron.lu@intel.com>
    Acked-by: "Huang, Ying" <ying.huang@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/nvdimm.sh
+++ lib/nvdimm.sh
@@ -1,16 +1,27 @@
 # nvdimm functions
 
+configure_namespace()
+{
+	ns=$1
+	bns=$(basename $ns)
+	rmode=$(cat "$ns/mode")
+	rsize=$(cat "$ns/size")
+	[ "$rsize" -eq 0 ] && return
+	rmode=$(echo -n $rmode)
+	mode=$(echo -n $mode)
+	[ "$rmode" = "$mode" ] && return
+	/lkp/benchmarks/bin/ndctl create-namespace --reconfig=$bns \
+					--force --mode="$mode" || exit 1
+}
+
 configure_nvdimm()
 {
-	for ns in $(ls -d /sys/bus/nd/devices/namespace*); do
-		bns=$(basename $ns)
-		rmode=$(cat "$ns/mode")
-		rsize=$(cat "$ns/size")
-		[ "$rsize" -eq 0 ] && continue
-		rmode=$(echo -n $rmode)
-		mode=$(echo -n $mode)
-		[ "$rmode" = "$mode" ] && continue
-		/lkp/benchmarks/bin/ndctl create-namespace --reconfig=$bns \
-						--force --mode="$mode" || exit 1
-	done
+	if [ -n "$ns_id" ]; then
+		ns=/sys/bus/nd/devices/namespace$ns_id.0
+		configure_namespace $ns
+	else
+		for ns in $(ls -d /sys/bus/nd/devices/namespace*); do
+			configure_namespace $ns
+		done
+	fi
 }
