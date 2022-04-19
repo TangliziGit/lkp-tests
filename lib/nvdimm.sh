c65615f5476ea1372552545ef37cb0d38c8b40eb
Aaron Lu <aaron.lu@intel.com>
2022-04-19 20:07:48 +0800

    setup/nvdimm: improve single namespace reconfig
    It appears the default mode of the namespaces after boot are random and
    when the default mode is raw, the current way to make only one specific
    namespace as raw will fail.
    
    To make this always work, introduce ns_id as well as ns_mode:
    
    nvdimm:
      mode: dax
      nr_dev: 1
      ns_id: 1
      ns_mode: raw
    
    In the above setup, all namespaces will be firstly configured to dax
    mode and then namespace1.0 will be reconfigured as raw mode. So after
    the setup, only /dev/pmem1 will appear, no /dev/pmem0.
    
    If ns_id and ns_mode are not specified, the existing behaviour is
    retained: all namespaces will be configured to $mode.
    
    "let's leave the echoes there for the sake of manual check"
    
    Cc: "Huang, Ying" <ying.huang@intel.com>
    Signed-off-by: Aaron Lu <aaron.lu@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/nvdimm.sh
+++ lib/nvdimm.sh
@@ -3,12 +3,14 @@
 configure_namespace()
 {
 	ns=$1
+	mode=$2
+
 	bns=$(basename $ns)
 	rmode=$(cat "$ns/mode")
 	rsize=$(cat "$ns/size")
+	echo "ns: $ns, mode: $mode, rmode: $rmode, rsize: $rsize"
 	[ "$rsize" -eq 0 ] && return
 	rmode=$(echo -n $rmode)
-	mode=$(echo -n $mode)
 	[ "$rmode" = "$mode" ] && return
 	/lkp/benchmarks/bin/ndctl create-namespace --reconfig=$bns \
 					--force --mode="$mode" || exit 1
@@ -16,12 +18,13 @@ configure_namespace()
 
 configure_nvdimm()
 {
+	for ns in $(ls -d /sys/bus/nd/devices/namespace*); do
+		configure_namespace $ns $(echo -n $mode)
+	done
+
 	if [ -n "$ns_id" ]; then
+		echo "ns_id: $ns_id"
 		ns=/sys/bus/nd/devices/namespace$ns_id.0
-		configure_namespace $ns
-	else
-		for ns in $(ls -d /sys/bus/nd/devices/namespace*); do
-			configure_namespace $ns
-		done
+		configure_namespace $ns $(echo -n $ns_mode)
 	fi
 }
