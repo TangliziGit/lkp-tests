7127c892eb110f2ecc445b3329956ab7ace74e77
Philip Li <philip.li@intel.com>
2022-04-24 16:13:32 +0800

    lib/bootstrap.sh: move __next_job to lib/tbox.sh
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/bootstrap.sh
+++ lib/bootstrap.sh
@@ -494,31 +494,6 @@ __reboot_bad_next_job()
 	reboot_tbox 2>/dev/null && exit
 }
 
-__next_job()
-{
-	NEXT_JOB="/tmp/next-job-$LKP_USER"
-
-	echo "getting new job..."
-	local mac="$(show_mac_addr)"
-	local last_kernel=
-	[ -n "$job" ] && last_kernel="last_kernel=$(escape_cgi_param $(grep ^kernel: $job | cut -d \" -f 2))&"
-	http_get_cgi "cgi-bin/gpxelinux.cgi?hostname=${HOSTNAME}&mac=$mac&${last_kernel}${manual_reboot}lkp_wtmp" \
-		$NEXT_JOB || {
-		echo "cannot get next job" 1>&2
-		set_tbox_wtmp 'cannot_get_next_job'
-		return 1
-	}
-
-	grep -q "^KERNEL " $NEXT_JOB || {
-		echo "no KERNEL found" 1>&2
-		set_tbox_wtmp 'no_kernel_found'
-		cat $NEXT_JOB
-		return 1
-	}
-
-	return 0
-}
-
 next_job()
 {
 	LKP_USER=${pxe_user:-lkp}
