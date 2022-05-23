TangliziGit/lkp-tests

commit 8674302d0a29f38aa18fca756c12f8af81c3faf1

lib: refactor to replace warn with log_warn
Signed-off-by: Philip Li <philip.li@intel.com>

lib/git_ext/base.rb
@@ -71,8 +71,7 @@ module Git
 
         [tag, false]
       else
-        warn "Not a kernel tree? Check #{repo}"
-        warn caller.join "\n"
+        log_warn e
 
         nil
       end

bin/proc-local
@@ -19,6 +19,7 @@ require "#{LKP_SRC}/lib/matrix"
 require "#{LKP_SRC}/lib/job"
 require "#{LKP_SRC}/lib/result_root"
 require "#{LKP_SRC}/lib/common"
+require "#{LKP_SRC}/lib/log"
 
 ENV['PATH'] = ENV['PATH'] + ":#{LKP_SRC}/bin"
 ENV['BENCHMARK_ROOT'] = '/lkp/benchmarks'
@@ -31,13 +32,13 @@ job = Job.new
 begin
   job.load(jobfile)
 rescue StandardError
-  warn "#{jobfile} is not a valid jobfile"
+  log_warn "#{jobfile} is not a valid jobfile"
   exit 1
 end
 
 _result_root = job._result_root
 if _result_root != File.dirname(result_root)
-  warn "_rt doesn't match rt: #{_result_root} vs. #{result_root}"
+  log_warn "_rt doesn't match rt: #{_result_root} vs. #{result_root}"
   exit 1
 end
 

bin/run-local
@@ -22,6 +22,7 @@ require "#{LKP_SRC}/lib/job2sh"
 require "#{LKP_SRC}/lib/job"
 require "#{LKP_SRC}/lib/result_root"
 require "#{LKP_SRC}/lib/common"
+require "#{LKP_SRC}/lib/log"
 
 ENV['PATH'] = ENV['PATH'] + ":#{LKP_SRC}/bin"
 ENV['BENCHMARK_ROOT'] = '/lkp/benchmarks'
@@ -81,7 +82,7 @@ def create_result_root(_result_root)
     FileUtils.mkdir_p result_root, mode: 0o2775
     return result_root
   end
-  warn "cannot create more result_roots under #{_result_root}"
+  log_warn "cannot create more result_roots under #{_result_root}"
   nil
 end
 
@@ -99,7 +100,7 @@ begin
     exit 1
   end
 rescue StandardError
-  warn "#{jobfile} is not a valid jobfile"
+  log_warn "#{jobfile} is not a valid jobfile"
   exit 1
 end
 

filters/need_kconfig.rb
@@ -4,6 +4,7 @@ LKP_SRC ||= ENV['LKP_SRC'] || File.dirname(__dir__)
 
 require 'yaml'
 require "#{LKP_SRC}/lib/kernel_tag"
+require "#{LKP_SRC}/lib/log"
 
 def read_kernel_version_from_context
   return nil unless self['kernel']
@@ -33,7 +34,7 @@ def check_kconfig(kconfig_lines, line)
   when /^(CONFIG_[A-Z0-9_]+=[ym])/, /^(CONFIG_[A-Z0-9_]+)/, /^(CONFIG_[A-Z0-9_]+=[0-9]+)/
     kconfig_lines =~ /^#{$1}/
   else
-    warn "unknown kconfig option: #{line}"
+    log_warn "unknown kconfig option: #{line}"
     true
   end
 end

lib/common.rb
@@ -7,6 +7,7 @@ require 'pathname'
 require 'fileutils'
 require 'stringio'
 require "#{LKP_SRC}/lib/array_ext"
+require "#{LKP_SRC}/lib/log"
 require 'English'
 
 def deepcopy(o)
@@ -257,7 +258,7 @@ def copy_and_decompress(src_fullpath, dst)
     src_fullpath += '.xz'
     %x(xz -cd #{src_fullpath} > #{dst})
   else
-    warn "File doesn't exist: #{src_fullpath}"
+    log_warn "File doesn't exist: #{src_fullpath}"
     return false
   end
 

lib/compare.rb
@@ -497,7 +497,7 @@ module Compare
         yield stat
       end
     rescue StandardError
-      warn "Error while comparing: #{mresult_roots.map(&:to_s).join ' '}"
+      log_warn "Error while comparing: #{mresult_roots.map(&:to_s).join ' '}"
       raise
     end
 

lib/dmesg.rb
@@ -6,6 +6,7 @@ require "#{LKP_SRC}/lib/yaml"
 require "#{LKP_SRC}/lib/constant"
 require "#{LKP_SRC}/lib/string_ext"
 require "#{LKP_SRC}/lib/lkp_path"
+require "#{LKP_SRC}/lib/log"
 
 LKP_SRC_ETC ||= LKP::Path.src('etc')
 
@@ -197,7 +198,7 @@ def grep_crash_head(dmesg_file)
       next
     end
 
-    warn "oops pattern mismatch: #{line}"
+    log_warn "oops pattern mismatch: #{line}"
   end
 
   oops_map

lib/lkp_git.rb
@@ -122,8 +122,8 @@ def __last_linus_release_tag(commit)
   elsif version == 2
     tag = "v2.#{patch_level}.#{sub_level}"
   else
-    warn "Not a kernel tree? check #{GIT_WORK_TREE}"
-    warn caller.join "\n"
+    log_warn "Not a kernel tree? check #{GIT_WORK_TREE}"
+    log_warn caller.join "\n"
     return nil
   end
 
@@ -232,7 +232,7 @@ def load_remotes
       repo_info['maintained_files'] ||= '*'
     end
 
-    warn "conflict repo name in different projects: #{remote}" if remotes[remote]
+    log_warn "conflict repo name in different projects: #{remote}" if remotes[remote]
 
     remotes[remote] = repo_info
   end

commit 3f51ce508759348b1e0b7c7f1fdb7ce6a40ada76

lib/git_ext/base.rb: fix "undefined local variable or method `e'"
lib/git_ext/base.rb:87:in `linux_last_release_tag_strategy': undefined local variable or method `e' for #<Git::Base:0x000056397d71b878> (NameError)

Fixes: 8674302d0a29 ("lib: refactor to replace warn with log_warn")
Signed-off-by: Philip Li <philip.li@intel.com>

lib/git_ext/base.rb
@@ -84,7 +84,7 @@ module Git
 
         [tag, false]
       else
-        log_warn e
+        log_warn "version #{version.inspect} of #{commit_sha} is invalid"
 
         nil
       end