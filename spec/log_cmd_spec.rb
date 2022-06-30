3a92422eb27b6f9cf9852e796155f7dcabbda985
Yujie Liu <yujie.liu@intel.com>
2022-06-30 22:11:53 +0800

    spec/log_cmd_spec.rb: silence unnecessary output
    == fix issue ==
    
    $ rake spec spec=log_cmd
    PWD = /home/yujie/upstream/lkp-tests
    ENV['LKP_SRC'] = /home/yujie/upstream/lkp-tests
    /usr/bin/ruby2.7 -I/var/lib/gems/2.7.0/gems/rspec-core-3.11.0/lib:/var/lib/gems/2.7.0/gems/rspec-support-3.11.0/lib /var/lib/gems/2.7.0/gems/rspec-core-3.11.0/exe/rspec --pattern spec/\*\*\{,/\*/\*\*\}/log_cmd_spec.rb
    2022-06-30 12:08:48 mkdir a b
    .2022-06-30 12:08:48 mkdir a b
    .2022-06-30 12:08:48 mkdir "a
    .2022-06-30 12:08:48 mkdir "a b"
    .2022-06-30 12:08:48 ls -n
    
    Signed-off-by: Yujie Liu <yujie.liu@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- spec/log_cmd_spec.rb
+++ spec/log_cmd_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 require 'tmpdir'
 require 'fileutils'
 require 'shellwords'
+require "#{LKP_SRC}/lib/bash"
 
 describe 'log_cmd' do
   let(:log_cmd) { "#{LKP_SRC}/bin/log_cmd " }
@@ -14,8 +15,8 @@ describe 'log_cmd' do
   end
 
   it 'creates multi dirs' do
-    was_good = system("#{log_cmd}mkdir a b")
-    expect(was_good).to be(true)
+    was_good = Bash.call2("#{log_cmd}mkdir a b")
+    expect(was_good).to be_truthy
     expect(Dir).to be_exist('a')
     expect(Dir).to be_exist('b')
     Dir.delete('a')
@@ -23,36 +24,36 @@ describe 'log_cmd' do
   end
 
   it 'creates dir with space' do
-    was_good = system("#{log_cmd}mkdir \"a b\"")
-    expect(was_good).to be(true)
+    was_good = Bash.call2("#{log_cmd}mkdir \"a b\"")
+    expect(was_good).to be_truthy
     expect(Dir).to be_exist('a b')
     Dir.delete('a b')
   end
 
   it 'creates dir with single quote' do
     dir = '"a'
-    was_good = system("#{log_cmd}mkdir #{Shellwords.escape(dir)}")
-    expect(was_good).to be(true)
+    was_good = Bash.call2("#{log_cmd}mkdir #{Shellwords.escape(dir)}")
+    expect(was_good).to be_truthy
     expect(Dir).to be_exist('"a')
     Dir.delete('"a')
   end
 
   it 'creates dir with space and double quotes' do
     dir = '"a b"'
-    was_good = system("#{log_cmd}mkdir #{Shellwords.escape(dir)}")
-    expect(was_good).to be(true)
+    was_good = Bash.call2("#{log_cmd}mkdir #{Shellwords.escape(dir)}")
+    expect(was_good).to be_truthy
     expect(Dir).to be_exist('"a b"')
     Dir.delete('"a b"')
   end
 
   it 'parses echo reserve parameter' do
-    was_good = system("#{log_cmd}ls -n")
-    expect(was_good).to be(true)
+    was_good = Bash.call2("#{log_cmd}ls -n")
+    expect(was_good).to be_truthy
   end
 
   it 'execute built-in command' do
-    was_good = system("#{log_cmd}cd .")
-    expect(was_good).to be(true)
+    was_good = Bash.call2("#{log_cmd}cd .")
+    expect(was_good).to be_truthy
   end
 
   after(:all) do
