TangliziGit/lkp-tests

commit 2bc66ad679e9ffc0be0a88a782f315f426685deb

lib/job.rb: use Bash call2 instead of system to execute shell command
call2 encapsulates capture3, and will give raw result once the command
fails. this could be more convenient for handling different output
stream and exitstatus.

Signed-off-by: Yujie Liu <yujie.liu@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/job.rb
@@ -663,9 +663,10 @@ class Job
   end
 
   def run_filter(_hash, _key, _val, script)
-    system @filter_env, script, unsetenv_others: true
-
-    raise Job::ParamError, "#{script}: exitstatus #{$CHILD_STATUS.exitstatus}" unless $CHILD_STATUS.success?
+    Bash.call2(@filter_env, script, unsetenv_others: true) do |stdout, _stderr, status|
+      puts stdout
+      raise Job::ParamError, "#{script}: exitstatus #{status}"
+    end
   end
 
   def expand_params(run_scripts: true)

commit 978bc576b930af6868f531eabdf77e8392a81149

lib/job.rb: add bash lib requirement
== fix issue ==

$ sudo lkp run ./hackbench-test-pipe-8-process-50%.yaml
/home/yujie/upstream/lkp-tests/lib/job.rb:666:in `run_filter': uninitialized constant Job::Bash (NameError)
	from /home/yujie/upstream/lkp-tests/lib/job.rb:694:in `block in expand_params'
	from /home/yujie/upstream/lkp-tests/lib/job.rb:79:in `block in for_each_in'
	from /home/yujie/upstream/lkp-tests/lib/job.rb:78:in `each'
	from /home/yujie/upstream/lkp-tests/lib/job.rb:78:in `for_each_in'
	from /home/yujie/upstream/lkp-tests/lib/job.rb:691:in `expand_params'
	from /home/yujie/upstream/lkp-tests/bin/run-local:137:in `<main>'

Fixes: 2bc66ad679e9 ("lib/job.rb: use Bash call2 instead of system to execute shell command")
Link: https://github.com/intel/lkp-tests/issues/144
Signed-off-by: Yujie Liu <yujie.liu@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/job.rb
@@ -9,6 +9,7 @@ require "#{LKP_SRC}/lib/constant"
 require "#{LKP_SRC}/lib/hash"
 require "#{LKP_SRC}/lib/erb"
 require "#{LKP_SRC}/lib/log"
+require "#{LKP_SRC}/lib/bash"
 require 'fileutils'
 require 'yaml'
 require 'json'