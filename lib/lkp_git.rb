TangliziGit/lkp-tests

commit 47605df7163d70cab393360641f8ead277dd44f1

lib/lkp_git.rb: parse remotes from LKP::Path.src
Signed-off-by: Chen Rong <rong.a.chen@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/lkp_git.rb
@@ -211,7 +211,7 @@ end
 
 def load_remotes
   remotes = {}
-  files = Dir["#{LKP_SRC}/repo/*/*"]
+  files = Dir[LKP::Path.src('repo', '*', '*')]
   files.each do |file|
     remote = File.basename file
     next if remote == 'DEFAULTS'

commit 3c16211dab5e0c581f5b316e5a50ae6fbf22205a

lib/lkp_git.rb: fix "in `load_remotes': uninitialized constant LKP::Path"
Fixes: 47605df7163d ("lib/lkp_git.rb: parse remotes from LKP::Path.src")
Signed-off-by: Philip Li <philip.li@intel.com>

lib/lkp_git.rb
@@ -16,6 +16,7 @@ require "#{LKP_SRC}/lib/cache"
 require "#{LKP_SRC}/lib/assert"
 require "#{LKP_SRC}/lib/git_ext"
 require "#{LKP_SRC}/lib/constant"
+require "#{LKP_SRC}/lib/lkp_path"
 
 GIT_WORK_TREE ||= ENV['GIT_WORK_TREE'] || ENV['LKP_GIT_WORK_TREE'] || "#{GIT_ROOT_DIR}/linux"
 GIT_DIR ||= ENV['GIT_DIR'] || "#{GIT_WORK_TREE}/.git"