567904f158992a4c3ddb065e25bdca30fe2457ee
Chen Rong <rong.a.chen@intel.com>
2022-08-10 18:09:13 +0800

    filters/need_kconfig.rb: move kernel_match_version? to kernel_tag.rb
    Signed-off-by: Chen Rong <rong.a.chen@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/kernel_tag.rb
+++ lib/kernel_tag.rb
@@ -30,3 +30,30 @@ class KernelTag
     numerized_kernel_version1 <=> numerized_kernel_version2
   end
 end
+
+def kernel_match_version?(kernel_version, expected_kernel_versions)
+  kernel_version = KernelTag.new(kernel_version)
+
+  expected_kernel_versions.all? do |expected_kernel_version|
+    match = expected_kernel_version.match(/(?<operator>==|!=|<=|>|>=)?\s*(?<kernel_tag>v[0-9]\.\d+(?:-rc\d+)*)/)
+    raise Job::SyntaxError, "Wrong syntax of kconfig setting: #{expected_kernel_versions}" if match.nil? || match[:kernel_tag].nil?
+
+    operator = match[:operator] || '>='
+
+    # rli9 FIXME: hack code to handle <=
+    # Take below example, MEMORY_HOTPLUG_SPARSE is moved in 5.16-rc1, thus we configure
+    # as <= 5.15. But we use rc_tag to decide the kernel of commit, 50f9481ed9fb or other
+    # commit now use kernel v5.15 to compare. This matches the <= and expects MEMORY_HOTPLUG_SPARSE
+    # is y, which leads to job filtered wrongly on these commits.
+    #
+    # fa55b7dcdc43 ("Linux 5.16-rc1")
+    # c55a04176cba ("Merge tag 'char-misc-5.16-rc1' ...")
+    # 50f9481ed9fb ("mm/memory_hotplug: remove CONFIG_MEMORY_HOTPLUG_SPARSE")
+    # 8bb7eca972ad ("Linux 5.15")
+    #
+    # To workaround this, change operator to < to mismatch the kernel
+    operator = '<' if operator == '<='
+
+    kernel_version.method(operator).call(KernelTag.new(match[:kernel_tag]))
+  end
+end
