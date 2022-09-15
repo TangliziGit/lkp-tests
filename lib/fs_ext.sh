TangliziGit/lkp-tests

commit 4f98faf31a369443b59c50e0adcc38fb09ea0e95

fio: run test on /lkp/benchmarks/fio if storage mounted on root
- If target partition is local storage (root partition, mounted on '/',
  where the OS is installed), fs will skip mounting steps and fio tests
  will write to /lkp/benchmarks/fio

Example:
- OS is installed in partition /dev/sda2 which means it's
mounted on root '/'
- In lkp-tests/hosts/<hostname>, "ssd_partitions: /dev/sda2" is
  specified
- fio tests can be split and run as usual and they will write
  to /lkp/benchmarks/fio

Signed-off-by: Lim, Lukaz Wei Hwang <lukaz.wei.hwang.lim@intel.com>

lib/fs_ext.sh
@@ -178,3 +178,9 @@ is_null_blk()
 	local dev=$1
 	[ $(echo "$dev" | grep -Fe "/null") ]
 }
+
+is_mount_on_root()
+{
+	local dev=$1
+	[ "$(df | grep $dev | grep -o '\S\+$')" = "/" ]
+}

setup/fio-setup-basic
@@ -187,6 +187,16 @@ for storage in $storages; do
 		storage_setup="filename=$storage"
 	elif is_null_blk /dev/$(basename $storage); then
 		storage_setup="filename=/dev/nullb0"
+	elif is_mount_on_root /dev/$(basename $storage); then
+		[ -d "/lkp/benchmarks/fio" ] || mkdir /lkp/benchmarks/fio
+		if [ "$testfile" = 'y' ]; then
+			storage_setup="\
+directory=/lkp/benchmarks/fio
+filename=fio_testfile_local
+"
+		else
+			storage_setup="directory=/lkp/benchmarks/fio"
+		fi
 	elif [ "$testfile" = 'y' ]; then
 		storage_setup="\
 directory=$storage

setup/fs
@@ -119,9 +119,9 @@ mount_fs() {
 	for dev in $bdevs
 	do
 		local mnt=/fs/$(basename $dev)
-		log_cmd mkdir -p $mnt
+		is_mount_on_root $dev || log_cmd mkdir -p $mnt
 		probe_filesystem $fs
-		if ! is_null_blk $dev; then
+		if ! is_null_blk $dev && ! is_mount_on_root $dev; then
 			log_cmd mount -t $fs ${mount:-$def_mount} $mount_option $dev $mnt || exit
 		fi
 		mount_points="${mount_points}$mnt "

commit af448ae1c64c47d9a85f9938e119a2e11bcbfde7

fio: fix check for root partition
If the name of root partition is a substring of another partition
(example: sda2 and sda21), 'grep $dev' will return more than 1
result and the check will fail, causing fio-setup-basic to not
write the fio testfile to /lkp/benchmarks/fio

Fixes: 4f98faf31a36 ("fio: run test on /lkp/benchmarks/fio if storage mounted on root")

Signed-off-by: Lim, Lukaz Wei Hwang <lukaz.wei.hwang.lim@intel.com>

lib/fs_ext.sh
@@ -182,5 +182,5 @@ is_null_blk()
 is_mount_on_root()
 {
 	local dev=$1
-	[ "$(df | grep $dev | grep -o '\S\+$')" = "/" ]
+	[ "$(df | grep -w $dev | grep -o '\S\+$')" = "/" ]
 }