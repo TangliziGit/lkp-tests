6273a72eceda1a0d2f0bd055ebf677e76ddab1fd
Chen Rong <rong.a.chen@intel.com>
2022-09-23 09:32:47 +0800

    lib/job.rb: update the pattern for saving job yaml
    The result of YAML.dump changed in new psych:
    
    psych (3.0.2):
    $ ruby -e 'require "yaml"; a={:"#! jobs/boot.yaml"=>nil, "suite"=>"boot"}; puts YAML.dump(a).split("\n").join("|")'
    ---|:#! jobs/boot.yaml: |suite: boot
    
    psych (3.3.1):
    $ ruby -e 'require "yaml"; a={:"#! jobs/boot.yaml"=>nil, "suite"=>"boot"}; puts YAML.dump(a).split("\n").join("|")'
    ---|:#! jobs/boot.yaml:|suite: boot
    
    Silence the below warning in `lkp run`:
    2022-09-20 14:07:06 +0000 WARN -- Jobfile is a dirty file, better to run 'lkp split-job --compatible fio-basic-local-disk-1M-1-1SSD-n-ext4-1-io_uring-1-300s-randrw-1g-y.yaml' first
    
    Signed-off-by: Chen Rong <rong.a.chen@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/job.rb
+++ lib/job.rb
@@ -123,8 +123,8 @@ def atomic_save_yaml_json(object, file)
     else
       lines = YAML.dump(object)
       # create comment lines from symbols
-      lines.gsub!(/^:#(.*): $/, "\n#\\1")
-      lines.gsub!(/^\? :#(.*)\n: $/, "\n#\\1")
+      lines.gsub!(/^:#(.*):( |)$/, "\n#\\1")
+      lines.gsub!(/^\? :#(.*)\n:( |)$/, "\n#\\1")
     end
     file.write(lines)
   end
