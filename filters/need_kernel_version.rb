TangliziGit/lkp-tests

commit 5dc2149d9ca859fc0601595f15ad73c1ec90a51d

filters/need_kernel_version.rb: support different version for gcc and clang
Failed to load module for old kernel if built with clang:
modprobe: ERROR: could not insert 'rcutorture': Exec format error

Signed-off-by: Chen Rong <rong.a.chen@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

filters/need_kernel_version.rb
@@ -12,7 +12,9 @@ def check_kernel_version
 
   context = YAML.load(File.read(context_file))
   kernel_version = context['rc_tag']
-  raise Job::ParamError, "kernel version not satisfied: #{kernel_version}" if kernel_version && !kernel_match_version?(kernel_version, Array(self['need_kernel_version']))
+  compiler = File.realpath(self['kernel']).include?('clang') ? 'clang' : 'gcc'
+  need_version = Array(self['need_kernel_version']).detect { |l| l.include?(compiler) }
+  raise Job::ParamError, "kernel version not satisfied: #{kernel_version}" if kernel_version && !kernel_match_version?(kernel_version, Array(need_version.split(',').first))
 end
 
 check_kernel_version if self['need_kernel_version'] && self['kernel']

include/locktorture
@@ -1,4 +1,6 @@
-need_kernel_version: '>= v4.17'
+need_kernel_version:
+- '>= v4.17, gcc'
+- '>= v5.12, clang'
 need_kconfig:
 - LOCK_TORTURE_TEST: m
 # LoadPin: kernel-module old-api-denied obj=<unknown> pid=731 cmdline="modprobe locktorture onoff_interval=3 onoff_holdoff=30"

include/rcurefscale
@@ -1,4 +1,6 @@
-need_kernel_version: '>= v4.17'
+need_kernel_version:
+- '>= v4.17, gcc'
+- '>= v5.12, clang'
 need_kconfig:
 - TORTURE_TEST: m
 - RCU_REF_SCALE_TEST: m, v5.9

include/rcuscale
@@ -1,4 +1,6 @@
-need_kernel_version: '>= v4.17'
+need_kernel_version:
+- '>= v4.17, gcc'
+- '>= v5.12, clang'
 need_kconfig:
 - RCU_PERF_TEST: m, <= v5.9
 - RCU_SCALE_TEST: m, v5.10-rc1

include/rcutorture
@@ -1,4 +1,6 @@
-need_kernel_version: '>= v4.17'
+need_kernel_version:
+- '>= v4.17, gcc'
+- '>= v5.12, clang'
 need_kconfig:
 - RCU_TORTURE_TEST: m
 - SECURITY_LOADPIN_ENABLED: n, <= v4.19

spec/filter_need_kernal_version_spec.rb
@@ -6,7 +6,6 @@ require "#{LKP_SRC}/lib/job"
 describe 'filter/need_kernel_version.rb' do
   before(:each) do
     @tmp_dir = Dir.mktmpdir(nil, '/tmp')
-    FileUtils.touch "#{@tmp_dir}/vmlinuz"
     FileUtils.chmod 'go+rwx', @tmp_dir
   end
 
@@ -14,18 +13,21 @@ describe 'filter/need_kernel_version.rb' do
     FileUtils.remove_entry @tmp_dir
   end
 
-  def generate_context(kernel_version)
-    File.open(File.join(@tmp_dir, 'context.yaml'), 'w') do |f|
+  def generate_context(compiler, kernel_version)
+    dir = File.join(@tmp_dir, compiler)
+    FileUtils.mkdir_p(dir)
+    FileUtils.touch("#{dir}/vmlinuz")
+    File.open(File.join(dir, 'context.yaml'), 'w') do |f|
       f.write({ 'rc_tag' => kernel_version }.to_yaml)
     end
   end
 
-  def generate_job(contents = "\n")
+  def generate_job(compiler, contents = "\n")
     job_file = "#{@tmp_dir}/job.yaml"
 
     File.open(job_file, 'w') do |f|
       f.puts contents
-      f.puts "kernel: #{@tmp_dir}/vmlinuz"
+      f.puts "kernel: #{@tmp_dir}/#{compiler}/vmlinuz"
     end
 
     # Job.open can filter comments (e.g. # support kernel xxx)
@@ -33,22 +35,30 @@ describe 'filter/need_kernel_version.rb' do
   end
 
   context 'kernel is not satisfied' do
-    it 'filters the job' do
-      generate_context('v4.16')
-      job = generate_job <<~EOF
-              need_kernel_version: '>= v4.17'
-      EOF
-      expect { job.expand_params }.to raise_error Job::ParamError
+    { 'v4.16' => 'gcc', 'v5.11' => 'clang' }.each do |version, compiler|
+      it "filters the job built with #{compiler}" do
+        generate_context(compiler, version)
+        job = generate_job compiler, <<~EOF
+              need_kernel_version:
+              - '>= v4.17, gcc'
+              - '>= v5.12, clang'
+        EOF
+        expect { job.expand_params }.to raise_error Job::ParamError
+      end
     end
   end
 
   context 'kernel is satisfied' do
-    it 'does not filters the job' do
-      generate_context('v5.0')
-      job = generate_job <<~EOF
-              need_kernel_version: '>= v4.17'
-      EOF
-      job.expand_params
+    { 'v5.0' => 'gcc', 'v5.12' => 'clang' }.each do |version, compiler|
+      it "does not filters the job built with #{compiler}" do
+        generate_context(compiler, version)
+        job = generate_job compiler, <<~EOF
+              need_kernel_version:
+              - '>= v4.17, gcc'
+              - '>= v5.12, clang'
+        EOF
+        job.expand_params
+      end
     end
   end
 end

commit 450b29f7110d32952da90e6884fb098a511e27da

filters/need_kernel_version.rb: fix `check_kernel_version': undefined method `split'
/lkp/lkp/src/filters/need_kernel_version.rb:17:in `check_kernel_version': undefined method `split' for nil:NilClass (NoMethodError)

Fixes: 5dc2149d9ca8 ("filters/need_kernel_version.rb: support different version for gcc and clang")
Signed-off-by: Philip Li <philip.li@intel.com>

filters/need_kernel_version.rb
@@ -14,7 +14,7 @@ def check_kernel_version
   kernel_version = context['rc_tag']
   compiler = File.realpath(self['kernel']).include?('clang') ? 'clang' : 'gcc'
   need_version = Array(self['need_kernel_version']).detect { |l| l.include?(compiler) }
-  raise Job::ParamError, "kernel version not satisfied: #{kernel_version}" if kernel_version && !kernel_match_version?(kernel_version, Array(need_version.split(',').first))
+  raise Job::ParamError, "kernel version not satisfied: #{kernel_version}" if need_version && kernel_version && !kernel_match_version?(kernel_version, Array(need_version.split(',').first))
 end
 
 check_kernel_version if self['need_kernel_version'] && self['kernel']
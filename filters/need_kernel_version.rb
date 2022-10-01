450b29f7110d32952da90e6884fb098a511e27da
Philip Li <philip.li@intel.com>
2022-10-01 20:27:34 +0800

    filters/need_kernel_version.rb: fix `check_kernel_version': undefined method `split'
    /lkp/lkp/src/filters/need_kernel_version.rb:17:in `check_kernel_version': undefined method `split' for nil:NilClass (NoMethodError)
    
    Fixes: 5dc2149d9ca8 ("filters/need_kernel_version.rb: support different version for gcc and clang")
    Signed-off-by: Philip Li <philip.li@intel.com>

--- filters/need_kernel_version.rb
+++ filters/need_kernel_version.rb
@@ -14,7 +14,7 @@ def check_kernel_version
   kernel_version = context['rc_tag']
   compiler = File.realpath(self['kernel']).include?('clang') ? 'clang' : 'gcc'
   need_version = Array(self['need_kernel_version']).detect { |l| l.include?(compiler) }
-  raise Job::ParamError, "kernel version not satisfied: #{kernel_version}" if kernel_version && !kernel_match_version?(kernel_version, Array(need_version.split(',').first))
+  raise Job::ParamError, "kernel version not satisfied: #{kernel_version}" if need_version && kernel_version && !kernel_match_version?(kernel_version, Array(need_version.split(',').first))
 end
 
 check_kernel_version if self['need_kernel_version'] && self['kernel']
