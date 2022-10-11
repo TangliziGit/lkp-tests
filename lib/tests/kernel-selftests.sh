e6e70db62e247a8155c241c489c68f2aa058bb7a
Yujie Liu <yujie.liu@intel.com>
2022-10-11 16:08:39 +0800

    lib/tests/kernel-selftests.sh: set 200s timeout for vm selftests
    == before fix ==
    
    # -------------------------------
    # running ./test_vmalloc.sh smoke
    # -------------------------------
    # Run smoke test. Note, this test provides basic coverage.
    # Please check ./test_vmalloc.sh output how it can be used
    # for deep performance analysis as well as stress testing.
    # Done.
    # Check the kernel ring buffer to see the summary.
    #
    not ok 4 selftests: vm: run_vmtests.sh # TIMEOUT 45 seconds
    
    == after fix ==
    
    # # Totals: pass:15 fail:0 xfail:0 xpass:0 skip:0 error:0
    # [PASS]
    ok 4 selftests: vm: run_vmtests.sh # SKIP
    
    Signed-off-by: Yujie Liu <yujie.liu@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/tests/kernel-selftests.sh
+++ lib/tests/kernel-selftests.sh
@@ -648,6 +648,9 @@ fixup_vm()
 		[[ $iterations -le 0 || ($nr_threads != "\$NUM_CPUS" && $nr_threads -le 0) ]] && die "Paramters: iterations or nr_threads must > 0"
 		sed -i 's/^STRESS_PARAM="nr_threads=$NUM_CPUS test_repeat_count=20"/STRESS_PARAM="nr_threads='$nr_threads' test_repeat_count='$iterations'"/' vm/test_vmalloc.sh
 	fi
+
+	# vm selftests may needs to run for more than 150s on some specific platforms and exceeds the default timeout 45s
+	echo 'timeout=200' > vm/settings
 }
 
 platform_is_skylake_or_snb()
