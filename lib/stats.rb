TangliziGit/lkp-tests

commit e29a467f7454b56d33030aed78def8f2e93c8138

lib/stats.rb: introduce samples_remove_early_fails to skip early failed rts
Signed-off-by: Chen Rong <rong.a.chen@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/stats.rb
@@ -518,6 +518,24 @@ def bisectable_stat?(stat)
   stat !~ $stat_denylist
 end
 
+def samples_remove_early_fails(matrix, samples, stat_boot_stage)
+  return samples if stat_boot_stage.zero?
+
+  perf_samples = []
+  samples.each_with_index do |v, i|
+    next if matrix['dmesg.bootstage:last'] &&
+            matrix['dmesg.bootstage:last'][i] != 0 &&
+            matrix['dmesg.bootstage:last'][i] < stat_boot_stage
+
+    next if matrix['kmsg.bootstage:last'] &&
+            matrix['kmsg.bootstage:last'][i] != 0 &&
+            matrix['kmsg.bootstage:last'][i] < stat_boot_stage
+
+    perf_samples << v
+  end
+  perf_samples
+end
+
 def samples_remove_boot_fails(matrix, samples)
   perf_samples = []
   samples.each_with_index do |v, i|

commit a976d71d6db83e92bd6615a70229d8f25ce95b0a

lib/stats.rb: fix samples_remove_early_fails: undefined method '<' for nil:NilClass
/zday/lkp/lib/stats.rb:528:in `block in samples_remove_early_fails': undefined method `<' for nil:NilClass (NoMethodError)
        from /zday/lkp/lib/stats.rb:525:in `each'
        from /zday/lkp/lib/stats.rb:525:in `each_with_index'
        from /zday/lkp/lib/stats.rb:525:in `samples_remove_early_fails'
        from /zday/lkp/lib/bisect.rb:168:in `valid_samples_for_bisect'
	from /zday/lkp/sbin/stats-good-or-bad:49:in `<main>'

Fixes: e29a467f7454 (lib/stats.rb: introduce samples_remove_early_fails to skip early failed rts)
Signed-off-by: Chen Rong <rong.a.chen@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/stats.rb
@@ -524,12 +524,12 @@ def samples_remove_early_fails(matrix, samples, stat_boot_stage)
   perf_samples = []
   samples.each_with_index do |v, i|
     next if matrix['dmesg.bootstage:last'] &&
-            matrix['dmesg.bootstage:last'][i] != 0 &&
-            matrix['dmesg.bootstage:last'][i] < stat_boot_stage
+            matrix['dmesg.bootstage:last'][i].to_i != 0 &&
+            matrix['dmesg.bootstage:last'][i].to_i < stat_boot_stage
 
     next if matrix['kmsg.bootstage:last'] &&
-            matrix['kmsg.bootstage:last'][i] != 0 &&
-            matrix['kmsg.bootstage:last'][i] < stat_boot_stage
+            matrix['kmsg.bootstage:last'][i].to_i != 0 &&
+            matrix['kmsg.bootstage:last'][i].to_i < stat_boot_stage
 
     perf_samples << v
   end