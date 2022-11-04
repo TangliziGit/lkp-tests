05477a2f730b24c232e31c356a264a39c0105db2
Wu Fengguang <wfg@mail.ustc.edu.cn>
2022-11-04 18:00:00 +0800

    install-dependencies.sh: fix errors in debian/fedora
    bash sbin/install-dependencies.sh
    /c/lkp-tests/lib/env.sh: line 143: hostname: command not found
    /c/lkp-tests/lib/env.sh: line 155: hostname: command not found
    /c/lkp-tests/lib/env.sh: line 155: hosts/: Is a directory
    /c/lkp-tests/lib/env.sh: line 162: hostname: command not found
    sbin/install-dependencies.sh: line 20: sudo: command not found
    sbin/install-dependencies.sh: line 51: gem: command not found
    sbin/install-dependencies.sh: line 52: sudo: command not found
    make: *** [Makefile:17: install] Error 127
    
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- sbin/install-dependencies.sh
+++ sbin/install-dependencies.sh
@@ -5,25 +5,29 @@ PROJECT_DIR=$(dirname $SCRIPT_DIR)
 
 . $PROJECT_DIR/lib/env.sh
 
+[ $(id -u) != 0 ] && SUDO=sudo
+
 # choose install function base on common Package Manager
 linux_dep()
 {
 	get_package_manager
 
+	local common_packages="ruby rubygems make gcc diffutils util-linux lftp hostname sudo gzip git"
+
 	case "$installer" in
 	apt-get)
 		export DEBIAN_FRONTEND=noninteractive
-		sudo "$installer" update
-		sudo "$installer" install -yqm ruby-dev libssl-dev gcc g++ uuid-runtime diffutils
+		$SUDO "$installer" update
+		$SUDO "$installer" install -yqm $common_packages ruby-dev libssl-dev g++ uuid-runtime
 		;;
 	dnf|yum)
-		sudo "$installer" install -y --skip-broken ruby rubygems gcc gcc-c++ make ruby-devel git lftp util-linux diffutils
+		$SUDO "$installer" install -y --skip-broken $common_packages gcc-c++ ruby-devel
 		;;
 	pacman)
-		sudo "$installer" -Sy --noconfirm --needed ruby rubygems gcc make git lftp util-linux diffutils
+		$SUDO "$installer" -Sy --noconfirm --needed $common_packages
 		;;
 	zypper)
-		sudo "$installer" install -y rubygems gcc gcc-c++ make ruby-devel git lftp util-linux hostname gzip diffutils
+		$SUDO "$installer" install -y $common_packages gcc-c++ ruby-devel
 		;;
 	*)
 		echo "Unknown Package Manager! please install dependencies manually." && exit 1
@@ -49,7 +53,7 @@ mac_dep()
 install_gem_pkg()
 {
 	gem sources -r https://rubygems.org/ -a https://gems.ruby-china.com/
-	sudo gem install -f git activesupport:6.1.4.4 rest-client faye-websocket md5sum base64
+	$SUDO gem install -f git activesupport:6.1.4.4 rest-client faye-websocket md5sum base64
 }
 
 run()
@@ -74,5 +78,5 @@ export CCI_REPOS=$(dirname $PWD)
 export PATH=\$PATH:\$LKP_SRC/sbin:\$LKP_SRC/bin"
 }
 
-set_env
 run
+set_env
