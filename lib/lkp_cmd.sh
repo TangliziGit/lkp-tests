fccd874fea8ba5e3e3d374b4f78061e2acb138fc
Wu Fengguang <wfg@mail.ustc.edu.cn>
2022-11-12 12:33:02 +0800

    avoid/reduce wait time
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- lib/lkp_cmd.sh
+++ lib/lkp_cmd.sh
@@ -3,8 +3,14 @@
 create_lkp_user() {
 	grep -q ^lkp: /etc/passwd && return
 
-	echo -n "Do you agree to create lkp users for testing? [Y/n] "
-	read -t 10 input
+	if is_docker || is_virt; then
+		# In docker/qemu, we can safely add lkp user.
+		input=y
+	else
+		echo -n "Do you agree to create lkp users for testing? [Y/n] "
+		read -t 10 input
+	fi
+
 	case $input in
 		N|n)
 			echo "error: lkp user is required to run lkp tests"
