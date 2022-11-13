a4114765533708ca041008d259d2a8539abf86bf
Wu Fengguang <wfg@mail.ustc.edu.cn>
2022-11-13 12:32:04 +0800

    lkp-install-run: reuse find_job()
    Signed-off-by: Wu Fengguang <wfg@mail.ustc.edu.cn>

--- lib/lkp_path.sh
+++ lib/lkp_path.sh
@@ -12,3 +12,25 @@ lkp_src()
 
 	echo "$lkp_src"
 }
+
+find_job()
+{
+	local job="$1"
+
+	if [ -f $job ]; then
+		job_file=$job
+		return
+	fi
+
+	job_file=($LKP_SRC/programs/*/jobs/$job)
+	[ -f "$job_file" ] && return
+
+	job_file=($LKP_SRC/jobs/$job)
+	[ -f "$job_file" ] && return
+
+	job_file=($LKP_SRC/jobs/*/$job)
+	[ -f "$job_file" ] && return
+
+	echo "Failed to find job $job"
+	return 1
+}
